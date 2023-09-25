12b98035f9d5bbfc7b41a129679dfccc199fb471
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2023-09-26 01:29:31 +0800

    add <Input>

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+type InputProps = {
+  value?: string,
+  type?: 'default' | 'danger' | 'primary',
+  primary?: boolean,
+  disable?: boolean,
+  href?: string,
+  className?: string,
+  onClick?: (e: any) => void,
+}
 
-const Input = () => {
+const buttonBodyColors: {[key: string]: string} = {
+  default: 'bg-slate-400',
+  primary: 'bg-blue-400',
+  danger: 'bg-red-400',
+}
+
+const buttonBorderColors: {[key: string]: string} = {
+  default: 'border-slate-400',
+  primary: 'border-blue-400',
+  danger: 'border-red-400',
+}
+
+
+const Input = ({
+  value='Button',
+  type='default',
+  primary=false,
+  disable=false,
+  href,
+  className,
+  onClick,
+} : InputProps) => {
+  const buttonBodyColor = buttonBodyColors[type]
+  const buttonBorderColor = buttonBorderColors[type]
+
+  const handleClick = (e: any) => {
+    if (href) { window.location.pathname = href }
+    if (onClick) { onClick(e) }
+  }
+
   return (
-    <div>Input</div>
+    <button
+      className={`rounded-full px-3 text-2xl border-2
+        ${primary ? buttonBodyColor : ''}
+        ${buttonBorderColor}
+        ${disable ? 'disable text-slate-400' : ''}
+        ${className ? className : ''}
+      `}
+      onClick={handleClick}
+    >
+      {value}
+    </button>
   )
 }
 
