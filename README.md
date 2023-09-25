e5d03c6528c9f74ed9949731f54c6f8d12818e04
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2023-09-26 01:45:44 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # XUI
-React Components Library
+An React UI library
+## ⭐️Features
+## 📦Install
+```
+npm install xui
+```
+## 🔨Usage
+```
+import { Button } from 'xui'
+
+const App = () => <Button>Hello</Button>
+```
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,55 +1,8 @@
-type InputProps = {
-  value?: string,
-  type?: 'default' | 'danger' | 'primary',
-  primary?: boolean,
-  disable?: boolean,
-  href?: string,
-  className?: string,
-  onClick?: (e: any) => void,
-}
+import React from 'react'
 
-const buttonBodyColors: {[key: string]: string} = {
-  default: 'bg-slate-400',
-  primary: 'bg-blue-400',
-  danger: 'bg-red-400',
-}
-
-const buttonBorderColors: {[key: string]: string} = {
-  default: 'border-slate-400',
-  primary: 'border-blue-400',
-  danger: 'border-red-400',
-}
-
-
-const Input = ({
-  value='Button',
-  type='default',
-  primary=false,
-  disable=false,
-  href,
-  className,
-  onClick,
-} : InputProps) => {
-  const buttonBodyColor = buttonBodyColors[type]
-  const buttonBorderColor = buttonBorderColors[type]
-
-  const handleClick = (e: any) => {
-    if (href) { window.location.pathname = href }
-    if (onClick) { onClick(e) }
-  }
-
+const Input = () => {
   return (
-    <button
-      className={`rounded-full px-3 text-2xl border-2
-        ${primary ? buttonBodyColor : ''}
-        ${buttonBorderColor}
-        ${disable ? 'disable text-slate-400' : ''}
-        ${className ? className : ''}
-      `}
-      onClick={handleClick}
-    >
-      {value}
-    </button>
+    <div>Input</div>
   )
 }
 
